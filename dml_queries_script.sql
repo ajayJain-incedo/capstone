85ab1f23542cd3a1ccf18aced1175b5a231b869b
prateek.joshi1 <prateek.joshi1@incedoinc.com>
2022-02-19 22:42:44 +0530

    Updated SQL Scripts

---
+++ dml_queries_script.sql
@@ -0,0 +1,84 @@
+## AFTER USER ADDS SOMETHING TO CART
+## Query for view cart [On the view cart page]
+
+select pname, product_id, quantity, amount
+from cart_item, product
+where user_id = 1001 and cart_item.product_id = product.pid ;
+
+
+
+## WHEN USER CLICKS ON BUY NOW BUTTON [On the view cart page]
+## The total amount for all cart_items will be inserted in orders table
+## The auto-generated order id will be inserted back to the cart table
+  
+## Procedure for inserting values from cart_item table into orders table 
+
+delimiter //
+create procedure insert_order(in uid int(5))
+begin
+declare total decimal(10,2);
+declare orderid int(5);
+select sum(amount) into total
+from cart_item 
+where user_id=uid;
+insert into orders(user_id,total_amount)
+values(uid,total);
+
+ select oid into orderid
+ from orders
+ where user_id = uid and payment_status = 'Pending';
+ 
+ update cart_item
+ set order_id = orderid
+ where user_id = uid;
+end; //
+
+call insert_order(uid);
+select * from orders;
+select * from cart_item;
+
+
+
+## AFTER CLICKING ON BUY NOW, USER SHALL COME TO PAYMENT PAGE AND SELECT PAY MODE
+## THEN USER SHALL CLICK ON PROCEED TO PAY
+## When user clicks on Pay, the payment status for corresponding order id will be updated to 'done' 
+## And The cart_item record for that order id will be removed and stored in the order_history table
+
+## Procedure for updating status and moving from cart_item table into order_history table [When user clicks on Proceed to Pay]
+
+delimiter //
+create procedure insert_history(in orderid int(5))
+begin
+update orders
+set payment_status = 'Done'
+where oid = orderid;
+
+insert into order_history (user_id,order_id,product_id,amount)
+select user_id,order_id,product_id,amount
+from cart_item 
+where order_id = orderid; 
+
+delete from cart_item
+where order_id = orderid;
+end; //
+
+call insert_history(4);
+select * from order_history
+
+
+## IF USER WANTS TO CHECK ORDER HISTORY
+## Query for viewing order history
+
+select pname, order_id, product_id, amount
+from order_history, product
+where user_id = 1001 and order_history.product_id = product.pid ;
+
+
+
+
+
+
+
+
+
+
--- dummy_insert_script.sql
+++ dummy_insert_script.sql
@@ -1,25 +1,28 @@
-insert into users(uid,first_name,last_name,email,pass,mobile,address) 
+## Test data for users table :
+
+insert into users(uid,first_name,last_name,email,pass,mobile,address)
 values(1001,'Rakesh','Sharma','rakesh21@gmail.com','r0123','9838868901','House 3,Shanti Nagar,Phase-4,gurgaon');
 
-insert into users(first_name,last_name,email,pass,mobile,address) 
+insert into users(first_name,last_name,email,pass,mobile,address)
 values('Rishi','Jain','rishijain1@gmail.com','rishi01','9837688901','House 4,Surya Nagar,lucknow');
 
-insert into users(first_name,last_name,email,pass,mobile,address) 
+insert into users(first_name,last_name,email,pass,mobile,address)
 values('Shreya','Sharma','shreya12@gmail.com','s9988','9235868901','House 12,arya colony,jaipur');
 
-insert into users(first_name,last_name,email,pass,mobile,address) 
+insert into users(first_name,last_name,email,pass,mobile,address)
 values('Harish','Jain','harish32@gmail.com','hj321','9838843201','house 6,hari nagar,jodhpur');
 
-insert into users(first_name,last_name,email,pass,mobile,address) 
+insert into users(first_name,last_name,email,pass,mobile,address)
 values('Ronak','Vasu','ronak01@gmail.com','vasu12','6678868901','house 1,vaishali apartment,Phase-4,noida');
 
-insert into users(first_name,last_name,email,pass,mobile,address,usertype) 
+insert into users(first_name,last_name,email,pass,mobile,address,usertype)
 values('Daksh','Ameta','dakshameta@gmail.com','daksh33','7908868901','A-403,Columbus Apartment,Phase-4,gurgaon','A');
 
 select * from users;
 
 
 
+## Test data for product table:
 
 insert into product(pid,pname,pdesc,price,category,available_quantity)
 values(1101,'Fit Protein','Healthy protein for adults',1099.50,'Supplements',100);
@@ -52,3 +55,31 @@ insert into product(pname,pdesc,price,category,available_quantity)
 values('Macbook M1 Pro','Apple 2021 macbook m1 pro',239999.99,'Supplements',30);
 
 select * from product;
+
+
+
+
+## Test data for cart_item table:
+## The order id column will be null initially and will get updated when user clicks on BUY NOW
+
+## Test 1
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1001,1101,2,2199.00);
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1002,1103,1,4999.00);
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1003,1110,1,239999.99);
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1001,1103,1,4999.00);
+## Test 2
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1001,1104,1,105999.00);
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1001,1103,1,4999.00);
+## Test 3
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1001,1110,1,239999.99);
+insert into cart_item(user_id,product_id,quantity,amount)
+values(1001,1103,1,4999.00);
+
+select * from cart_item;
